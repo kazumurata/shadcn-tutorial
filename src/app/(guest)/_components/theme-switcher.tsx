90c5fbf6a62155d01d1dea5cdcdf88f192f3d8c2
kazumurata <murata.kazuhiro.public@gmail.com>
2025-05-15 02:44:48 +0900

    header

---
+++ src/app/(guest)/_components/theme-switcher.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Button } from '@/shared/components/button';
+import { MoonIcon, SunIcon } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import { useCallback } from 'react';
+
+export default function ThemeSwitcher() {
+  const { resolvedTheme, setTheme } = useTheme();
+
+  const handleChangeTheme = useCallback(() => {
+    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+  }, [resolvedTheme, setTheme]);
+
+  return (
+    <Button
+      variant={'ghost'}
+      onClick={handleChangeTheme}
+      className="group/toggle h-8 w-8 px-0"
+    >
+      <SunIcon className="hidden dark:block" />
+      <MoonIcon className="block dark:hidden" />
+    </Button>
+  );
+}
--- src/app/(guest)/layout.tsx
+++ src/app/(guest)/layout.tsx
@@ -1,11 +1,15 @@
 import AppBreadcrumb from '@/app/(guest)/_components/app-breadcrumb';
 import AppSidebar from '@/app/(guest)/_components/app-sidebar';
+import { GithubIcon } from '@/app/(guest)/_components/icons';
+import ThemeSwitcher from '@/app/(guest)/_components/theme-switcher';
+import { Button } from '@/shared/components/button';
 import { Separator } from '@/shared/components/separator';
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from '@/shared/components/sidebar';
+import Link from 'next/link';
 import { ReactNode } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
@@ -15,11 +19,30 @@ export default function Layout({ children }: { children: ReactNode }) {
       <AppSidebar />
       <SidebarInset>
         {/* ヘッダー */}
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <header className="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          {/* 左側 */}
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation={'vertical'} className="mr-2 h-4" />
             <AppBreadcrumb />
+          </div>
+          {/* 右側 */}
+          <div className="flex items-center gap-0.5 px-4">
+            <Button
+              asChild
+              variant={'ghost'}
+              size={'icon'}
+              className="h-8 w-8 px-0"
+            >
+              <Link
+                href={'https://github.com/kazumurata/shadcn-tutorial'}
+                target={'_blank'}
+                rel={'noreferrer'}
+              >
+                <GithubIcon className="h-4 w-4" />
+              </Link>
+            </Button>
+            <ThemeSwitcher />
           </div>
         </header>
         <Separator />
